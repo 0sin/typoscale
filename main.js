048b5ea4539d088c395c1f8bf717ed158c472bad
0sin <69961808+0sin@users.noreply.github.com>
2021-07-03 01:13:13 +0900

    Update select box

--- main.js
+++ main.js
@@ -1,21 +1,21 @@
-const selectFontStyle = document.querySelector('.select-box');
+// const selectFontStyle = document.querySelector('.select-box');
 
-const optionItems = document.querySelector('.option-items');
-const optionItem = document.querySelectorAll('.option-item');
+
+// const optionItem = document.querySelectorAll('.option-item');
 
 // selectFontStyle.addEventListener('click', function() {
-//   optionItemsBox.classList.toggle('open');
+//   optionItemsButton.classList.toggle('open');
 //   optionItems.classList.toggle('open');
 // })
 
-optionItem.forEach(e => {
-  e.addEventListener('click', () => {
-    const selectedItemText = document.querySelector('.selected-item');
-    selectedItemText.innerHTML = e.innerHTML;
-    optionItem.forEach(e => e.classList.remove('active'));
-    e.classList.add('active');
-  })
-});
+// optionItem.forEach(e => {
+//   e.addEventListener('click', () => {
+//     const selectedItemText = document.querySelector('.selected-item');
+//     selectedItemText.innerHTML = e.innerHTML;
+//     optionItem.forEach(e => e.classList.remove('active'));
+//     e.classList.add('active');
+//   })
+// });
 
 
 // optionItems.addEventListener('mousemove', () => {
@@ -23,15 +23,29 @@ optionItem.forEach(e => {
 // })
 
 
-document.addEventListener('click', e => {
-  const optionItemsBox = document.querySelector('.selected-item-box');
-  console.log(e);
 
-  if(e.target.matches('.selected-item-box')) {
-    optionItemsBox.classList.toggle('open');
+// 1. 셀렉트 클릭 이벤트
+document.addEventListener('click', e => {
+  const optionItemsButton = document.querySelector('.selected-item-button');
+  const optionItems = document.querySelector('.option-items');
+  const optionItem = document.querySelectorAll('.option-item');
+  const selectedItem = document.querySelector('.selected-item');
+
+  // 셀렉트 박스 클릭-오픈
+  if(e.target.matches('.selected-item')) { //화살표 추가하기
+    optionItemsButton.classList.toggle('open');
     optionItems.classList.toggle('open');
   } else {
-    optionItemsBox.classList.remove('open');
+    optionItemsButton.classList.remove('open');
     optionItems.classList.remove('open');
+  }
+
+  // 옵션 아이템 선택-아이템 텍스트 전달
+  if(e.target.matches('.option-item')) {
+    const selectItem = e.target;
+    selectedItem.textContent = selectItem.textContent.trim();
+    // optionItem.forEach(el => console.log(el));
+    optionItem.forEach(el => el.classList.remove('active'));
+    selectItem.classList.add('active');
   }
 })
